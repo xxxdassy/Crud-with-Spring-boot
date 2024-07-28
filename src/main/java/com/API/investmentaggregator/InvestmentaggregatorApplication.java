c68c5837e4751dcc6ee428e7ae7c2fc219acfbd4
dassy <professionaldassy@gmail.com>
2024-07-28 16:01:23 -0300

    chore: init project

---
+++ src/main/java/com/API/investmentaggregator/InvestmentaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.API.investmentaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API/investmentaggregator/InvestmentaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.investmentaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
